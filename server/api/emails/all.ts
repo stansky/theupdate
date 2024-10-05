44d25c211fceea90bc27c4c69245e5fd48824cc1
Michael Stanescu <mike@kwole.com>
2024-10-04 21:17:11 -0700

    test

--- server/api/emails/all.ts
+++ server/api/emails/all.ts
@@ -4,6 +4,10 @@ import mysql from 'mysql2/promise'
 export default defineEventHandler(async (event) => {
   const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB_NAME } = process.env;
 
+  console.log('MYSQL_HOST:', process.env.MYSQL_HOST);
+  console.log('MYSQL_USER:', process.env.MYSQL_USER);
+  console.log('MYSQL_DB_NAME:', process.env.MYSQL_DB_NAME);
+
   // Create MySQL connection
   const connection = await mysql.createConnection({
     host: MYSQL_HOST,
